a32d78db1c76e28b4de490b1e41eb8b86e59506d
n8slab <nate.equan@gmail.com>
2025-02-07 14:17:51 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (46.32%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,44 +1,13 @@
 import java.lang.Object;
 class Solution {
     public int removeElement(int[] nums, int val) {
-        //skip null case exception
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        //set pointers
-        int len = nums.length;
-        int j = len - 1;
-
-        //start counter for number of val occurences
-        int k = 0;
-
-        for (int i = 0; i < len; i++) {
-            if (nums[i] == val) {
-                //increment counter
-                k++;
-                //close loop if thats the last index
-                if (j == i) {
-                    return len - k;
-                }
-                //check if last element is equal to the val to be removed, then work backwards
-                while (nums[j] == val) {
-                    //update the end pointer to the next value to be checked. Update occurence pointer
-                    j--;
-                    k++;
-                    //end loop if we're done searching all the indexes
-                    if (j == i) {
-                        return len - k;
-                    }
-                }
-                //swap the pointers before loop repeats
-                nums[i] = nums[j];
-                j--;
-            }
-            if (i == j) {
-                return len - k;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return len - k;
+        return index;
     }
 }
