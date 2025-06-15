d08ba2d1ca32b3f3e76e933390b2a13682973b03
n8slab <nate.equan@gmail.com>
2025-06-15 11:05:36 -0400

    Time: 7 ms (57.78%) | Memory: 19.4 MB (19.51%) - LeetSync

---
+++ LeetCode_Solutions/Java/169-majority-element/majority-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        x = None 
+        c = 0
+        m = len(nums)/2
+
+        for num in nums:
+            if c == 0:
+                x = num
+            c += (1 if num==x else -1)
+
+        return x
