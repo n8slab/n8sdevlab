9ac49c4dfe7441fa0d71273fac06bc6755d06307
n8slab <nate.equan@gmail.com>
2025-08-21 00:46:49 -0400

    Time: 100 ms (32.06%) | Memory: 18.5 MB (97.14%) - LeetSync

--- LeetCode_Solutions/Java/643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ LeetCode_Solutions/Java/643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -8,8 +8,15 @@ class Solution(object):
         curSum = sum(nums[:k])
         curMax = curSum
 
-        for i in range(k, len(nums)):
-            curSum += (nums[i] - nums[i-k])
+        #for i in range(k, len(nums)):
+        #    curSum += (nums[i] - nums[i-k])
+        #    curMax = max(curMax, curSum)
+        l = 0
+        r = k
+        while r < len(nums):
+            curSum += (nums[r] - nums[l])
             curMax = max(curMax, curSum)
-        
+            l +=1
+            r +=1
+
         return curMax/float(k)
