61a8ab98e1efce12478882b859b561fedfc0773a
n8slab <nate.equan@gmail.com>
2025-08-31 19:21:15 -0400

    Time: 2154 ms (47.26%) | Memory: 12.4 MB (96.24%) - LeetSync

--- LeetCode_Solutions/Java/37-sudoku-solver/sudoku-solver.py
+++ LeetCode_Solutions/Java/37-sudoku-solver/sudoku-solver.py
@@ -4,40 +4,40 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
-        empties = []
-
+        r = [0] * 9
+        c = [0] * 9
+        b = [0] * 9
+        e_c = []
         for i in range(9):
             for j in range(9):
-                if board[i][j] == '.':
-                    empties.append((i, j))
+                if board[i][j] != '.':
+                    num = int(board[i][j])
+                    test = 1 << (num - 1)
+                    r[i] |= test
+                    c[j] |= test
+                    b[(i // 3) * 3 + (j // 3)] |= test
                 else:
-                    num = board[i][j]
-                    rows[i].add(num)
-                    cols[j].add(num)
-                    boxes[(i // 3) * 3 + j // 3].add(num)
+                    e_c.append((i, j))
 
-        def backtrack(k=0):
-            if k == len(empties):
+        def backtrack(index):
+            if index == len(e_c):
                 return True
-            r, c = empties[k]
-            b = (r // 3) * 3 + c // 3
-            for num in "123456789":
-                if num not in rows[r] and num not in cols[c] and num not in boxes[b]:
-                    board[r][c] = num
-                    rows[r].add(num)
-                    cols[c].add(num)
-                    boxes[b].add(num)
-
-                    if backtrack(k + 1):
+            i, j = e_c[index]
+            b_i = (i // 3) * 3 + (j // 3)
+            used_numbers = r[i] | c[j] | b[b_i]
+            for num in range(1, 10):
+                test = 1 << (num - 1)
+                if not (used_numbers & test):
+                    board[i][j] = str(num)
+                    r[i] |= test
+                    c[j] |= test
+                    b[b_i] |= test
+                    if backtrack(index + 1):
                         return True
-
-                    board[r][c] = '.'
-                    rows[r].remove(num)
-                    cols[c].remove(num)
-                    boxes[b].remove(num)
+                    board[i][j] = '.'
+                    r[i] ^= test
+                    c[j] ^= test
+                    b[b_i] ^= test
             return False
 
-        backtrack()
+        backtrack(0)
