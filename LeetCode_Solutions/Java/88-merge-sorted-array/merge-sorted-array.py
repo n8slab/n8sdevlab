9f64b0bf11a0120e4356a1173dfab7af95f98c03
n8slab <nate.equan@gmail.com>
2026-06-01 09:56:51 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (76.86%) - LeetSync

--- LeetCode_Solutions/Java/88-merge-sorted-array/merge-sorted-array.py
+++ LeetCode_Solutions/Java/88-merge-sorted-array/merge-sorted-array.py
@@ -3,19 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m - 1  # nums1's index (the actual nums)
-        j = n - 1  # nums2's index
-        k = m + n - 1  # nums1's index (the next filled position)
+        i = m-1
+        j = n-1
+        k = m + n - 1
 
-        while j >= 0:
+        while j>=0:
             if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
-                k -= 1
-                i -= 1
+                k-=1
+                i-=1
             else:
                 nums1[k] = nums2[j]
-                k -= 1
-                j -= 1
-
-
+                k-=1
+                j-=1
         
